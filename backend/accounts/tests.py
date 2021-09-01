979e1d8c1e5b9dc1f567a2143655b08ab4030a80
George Johnson <georgemjohnson11@gmail.com>
2021-08-31 18:14:57 -0600

    Install pytest for unit tests Setup mock file for pytest Setup a pytest.ini

--- backend/accounts/tests.py
+++ backend/accounts/tests.py
@@ -1,3 +1,27 @@
 from django.test import TestCase
 
 # Create your tests here.
+def test_user_create():
+    print('test')
+
+
+def test_user_account_verification():
+    print('test')
+
+
+def test_user_web_token():
+    print('test')
+
+
+def test_user_refresh_token():
+    print('test')
+
+
+def test_user_request_new_password():
+    print('test')
+
+
+def test_user_confirm_new_password():
+    print('test')
+
+
